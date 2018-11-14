c1b31a8daab5061d0c3a6413e66d7f25d80c9195
Emrys West <westemrys@gmail.com>
2018-11-14 14:14:25 -0500

    back to start

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,81 +1,11 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import Images from '/lib/images.collection.js';
-import { Tasks } from '../api/tasks.js';
 
 import './body.html';
 
-Template.uploadedFiles.helpers({
-  uploadedFiles: function () {
-    return Images.find();
-  }
-});
-
-Template.uploadForm.onCreated(function () {
-  this.currentUpload = new ReactiveVar(false);
-});
-
-Template.uploadForm.helpers({
-  currentUpload: function () {
-    return Template.instance().currentUpload.get();
-  }
-});
-
-Template.uploadForm.events({
-  'change #fileInput': function (e, template) {
-    if (e.currentTarget.files && e.currentTarget.files[0]) {
-      // We upload only one file, in case
-      // there was multiple files selected
-      var file = e.currentTarget.files[0];
-      if (file) {
-        var uploadInstance = Images.insert({
-          file: file,
-          streams: 'dynamic',
-          chunkSize: 'dynamic'
-        }, false);
-
-        uploadInstance.on('start', function() {
-          template.currentUpload.set(this);
-        });
-
-        uploadInstance.on('end', function(error, fileObj) {
-          if (error) {
-            window.alert('Error during upload: ' + error.reason);
-          } else {
-            window.alert('File "' + fileObj.name + '" successfully uploaded');
-          }
-          template.currentUpload.set(false);
-        });
-
-        uploadInstance.start();
-      }
-    }
-  }
-});
-
-
 Template.body.helpers({
-  tasks() {
-    return Tasks.find({});
-  },
-});
-
-Template.body.events({
-  'submit .new-task'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
-
-    // Get value from form element
-    const target = event.target;
-    const text = target.text.value;
-
-    // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
-
-    // Clear form
-    target.text.value = '';
-  },
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
 });
--- server/main.js
+++ server/main.js
@@ -1,7 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import fs from 'fs';
-import Images from '/lib/images.collection.js';
-import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
